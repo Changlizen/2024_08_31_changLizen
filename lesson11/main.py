9ae976568b66cf6afa53769fa3e0ad70bb9625e8
張立人 <lizen941010@gmail.com>
2024-10-05 16:16:02 +0800

    lesson11_8

--- lesson11/main.py
+++ lesson11/main.py
@@ -8,6 +8,8 @@ led->gpio15
 '''
 
 from machine import Timer,ADC,Pin,PWM,RTC
+import binascii
+from umqtt.simple import MQTTClient
 import tools
 
 
@@ -33,7 +35,9 @@ def do_thing1(t):
 
     duty = adc1.read_u16()
     pwm.duty_u16(duty)    
-    print(f'可變電阻:{round(duty/65535*10)}')
+    light_level = round(duty/65535*10)
+    print(f'可變電阻:{light_level}')
+    mqtt.publish('SA-39/LIGHT_LEVEL', f'{light_level}')
 
 
 def main():
@@ -49,9 +53,16 @@ def main():
         t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
         t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
 
+
 if __name__ == '__main__':
     adc = ADC(4) #內建溫度
     adc1 = ADC(Pin(26)) #可變電阻
     adc_light = ADC(Pin(28)) #光敏電阻
     pwm = PWM(Pin(15),freq=50) #pwm led
+
+    #MQTT
+    SERVER = "192.168.0.252"
+    CLIENT_ID = binascii.hexlify(machine.unique_id())
+    mqtt = MQTTClient(CLIENT_ID, SERVER,user='pi',password='raspberry')
+    mqtt.connect()
     main()
